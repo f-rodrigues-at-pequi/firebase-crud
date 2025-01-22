3596f4950667d2d70c16d484b5342d706a02b460
Frederico Rodrigues <f.rodrigues@Accountss-MacBook-Air.local>
2025-01-22 18:13:45 -0300

    init new form

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
     }
   }
 
-  async googleSignin(){
+  async googleSignIn(){
     try {
       const provider = new firebase.auth.GoogleAuthProvider();
       const credential = await this.auth.signInWithPopup(provider);
--- src/app/root/root.component.ts
+++ src/app/root/root.component.ts
@@ -1,10 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../auth.service';
+import { map, Observable } from 'rxjs';
+import { AngularFireDatabase, SnapshotAction } from '@angular/fire/compat/database';
+
+export interface Item {
+  key: string;
+  email: string;
+  name: string;
+  age: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: false,
-
+ 
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css']
 })
@@ -12,9 +21,35 @@ export class RootComponent implements OnInit {
   email = '';
   password = '';
 
-  constructor(public auth: AuthService) {}
+  formNewName = '' as string;
+  formNewAge = '' as string;
 
-  ngOnInit(): void {
+  listRef: any;
+  list: Observable<Item[]>;
 
+  constructor(public auth: AuthService, database: AngularFireDatabase) {
+    this.listRef = database.list('items');
+    this.list = this.listRef.snapshotChanges().pipe(
+      map((changes: SnapshotAction<Item>[]) =>
+        changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    )
+  }
+
+  ngOnInit(): void {}
+
+  addItem() {
+    this.listRef.push({
+      nome: this.formNewName,
+      idade: this.formNewAge,
+      email: this.email,
+    })
+
+    this.formNewName = '';
+    this.formNewAge = '';
+  }
+
+  deleteItem(key: string) {
+    this.listRef.remove(key); 
   }
 }
